1a8b613271fc69f9872e0bc4a23c46d89c68d6a9
sochetanie <allagorik2901@gmail.com>
2017-08-03 16:33:58 -0400

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Bands extends Component {
   render(){
+  // debugger
     return (
       <div>
-        band input
+        <ul>
+          {this.props.store.getState().bands.map((band, i) => {
+            return <li key={i}>{band}</li>}
+          )}
+        </ul>
       </div>
-    );
+    )
   }
-};
+}
 
-export default Bands;
+export default Bands
